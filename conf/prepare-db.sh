931b8177461e50a71d4aaca80108eed82fe6fe7e
Zhan.Shi <g.shizhan.g@gmail.com>
2018-09-29 13:45:23 +0800

    monitoring suite

---
+++ conf/prepare-db.sh
@@ -0,0 +1,1 @@
+curl -i -XPOST http://localhost:8086/query --data-urlencode "q=CREATE DATABASE graphite"
---
+++ conf/start-collectl.sh
@@ -0,0 +1,2 @@
+collectl -i1 -scdmnstxyCDMNTXY --dskfilt sd -f /vagrant_data/collectl --rawtoo --export graphite,192.168.33.127:2003,d=1,s=cdmnstxyCDMNTXY
+

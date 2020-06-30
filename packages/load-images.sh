c1de73282f77899e2c1c06fb0a46d63e9909c3af
Shi.Zhan <g.shizhan.g@gmail.com>
2020-06-30 23:01:29 +0800

    check before load

--- packages/load-images.sh
+++ packages/load-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ $HOSTNAME = "controller" ]; then
-    docker load -i grafana_grafana_7.0.4.tar
-    docker load -i prom_prometheus_v2.19.2.tar
+    [ -f grafana_grafana_7.0.4.tar ]   && docker load -i grafana_grafana_7.0.4.tar   || docker pull grafana/grafana:7.0.4
+    [ -f prom_prometheus_v2.19.2.tar ] && docker load -i prom_prometheus_v2.19.2.tar || docker pull prom/prometheus:v2.19.2
 else
-    docker load -i alpine_3.12.0.tar
+    [ -f alpine_3.12.0.tar ] && docker load -i alpine_3.12.0.tar || docker pull alpine:3.12.0
 fi

9e3dc5d83c15e91347bfe6aeb866b3c817545650
Zhan.Shi <g.shizhan.g@gmail.com>
2020-08-19 07:56:36 +0800

    prepare images

--- packages/prepare-k8s-images.sh
+++ packages/prepare-k8s-images.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-for i in `kubeadm config images list`; do
-    imageName=${i#k8s.gcr.io/}
+for image in `kubeadm config images list --config /home/vagrant/conf/kubeadm.conf`; do
+    imageName=${image#registry.aliyuncs.com/google_containers/}
     imageFile="/packages/k8s.gcr.io_${imageName%:*}.tar"
     if [ -f $imageFile ]; then
         docker load -i $imageFile
     else
-        docker pull registry.aliyuncs.com/google_containers/$imageName
-        docker tag registry.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
-        docker rmi registry.aliyuncs.com/google_containers/$imageName
+        docker pull $image
+        docker tag $image k8s.gcr.io/$imageName
         docker save k8s.gcr.io/$imageName -o $imageFile
     fi
 done

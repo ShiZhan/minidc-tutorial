692c8aecf2d82bf788eb97171e2dbc276a1e423d
Shi.Zhan <g.shizhan.g@gmail.com>
2020-08-30 23:03:50 +0800

    prepare test container for k8s

--- packages/install-node.sh
+++ packages/install-node.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # testing container
-[ -f alpine_3.12.0.tar ] && docker load -i alpine_3.12.0.tar || docker pull alpine:3.12.0
+[ -f alpine~3.12.0.tar ]       && docker load -i alpine~3.12.0.tar       || docker pull alpine:3.12.0
+[ -f nginx~1.19.2-alpine.tar ] && docker load -i nginx~1.19.2-alpine.tar || docker pull nginx:1.19.2-alpine
 
 # node exporter
 PREFIX="https://mirrors.aliyun.com/ubuntu/pool/universe/p/"
